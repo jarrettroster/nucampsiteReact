44504fcd17bbfc2b3b309efb221ae1f860ab54e0
Jarrett Roster <jarrettroster@protonmail.com>
2023-03-24 19:36:52 -0600

    Router Setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+        <Routes>
+          <Route path='/' element={<HomePage />} /> 
+          <Route path='contact' element={<ContactPage />} />
+          <Route path='directory' element={<CampsitesDirectoryPage />} /> 
+        </Routes>
       <Footer />
     </div>
   );
